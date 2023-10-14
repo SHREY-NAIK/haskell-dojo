cd314d490530671549558e71fed3189d6a1b0489
SHREY NAIK <shrey.naik@gmail.com>
2023-10-14 19:16:09 +0000

    Assignment 1

--- src/TTT/A1.hs
+++ src/TTT/A1.hs
@@ -3,49 +3,83 @@ module TTT.A1 where
 import Data.Char (toUpper)
 
 -- Q#01
-
-_SIZE_ = undefined
+_SIZE_ :: Int
+_SIZE_ = 3
 
 -- Q#02
-
-_DISPLAY_LOGO_ = undefined
+_DISPLAY_LOGO_ :: Bool
+_DISPLAY_LOGO_ = True
 
 -- Q#03
-
-convertRowIndex = undefined
+convertRowIndex :: Char -> Int
+convertRowIndex char = fromEnum (toUpper char) - 65
 
 -- Q#04
 
-_INVALID_MOVE_ = undefined
+_INVALID_MOVE_ :: (Int, Int)
+_INVALID_MOVE_ = (-1, -1)
+
 
 -- Q#05
+_SEP_ :: String
+_SEP_ = "_|_"
 
-_SEP_ = undefined
 
 -- Q#06
 
-data Square
+data Square = X | O | Empty
+  deriving (Show, Eq)
+
 
 -- Q#07
 
-data GameState
+data GameState = XWon | OWon | Tie | InProgress
+  deriving (Show, Eq)
+
 
 -- Q#08
+type Player = Square
+type Row = [Square]
+type Line = [Square]
+type Board = [Row]
+type Move = (Int, Int)
+
 
 -- Q#09
+-- Using if-then-else
+getFirstPlayer :: Bool -> Player
+getFirstPlayer coinFlipResult =
+    if coinFlipResult then X else O
 
-getFirstPlayer = undefined
 
-getFirstPlayer_ = undefined
+-- Using guards
+getFirstPlayer_ :: Bool -> Player
+getFirstPlayer_ coinFlipResult
+    | coinFlipResult = X
+    | otherwise = O
+
 
 -- Q#10
 
-showGameState = undefined
+showGameState :: GameState -> String
+showGameState gameState =
+    case gameState of
+        XWon -> "Player X won the game!"
+        OWon -> "Player O won the game!"
+        Tie -> "The game is a tie!"
+        InProgress -> "The game is in progress."
+
 
 -- Q#11
 
-switchPlayer = undefined
+switchPlayer :: Player -> Player
+switchPlayer X = O
+switchPlayer O = X
+switchPlayer Empty = Empty
+
 
 -- Q#12
-
-showSquare = undefined
+showSquare :: Square -> String
+showSquare X = "X"
+showSquare O = "O"
+showSquare Empty = "_"
